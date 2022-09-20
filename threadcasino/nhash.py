310d5b72b2bebc0f10c3714e711d64847f4bce4f
John White <91273156+BoltR6@users.noreply.github.com>
2022-09-20 10:11:51 -0500

    Create nhash.py

---
+++ threadcasino/nhash.py
@@ -0,0 +1,4 @@
+def encrypt (s,m): return int(s * m + m)
+def decrypt (s,m): return int( (s - m) / m )
+# this has to be the most secure thing ever right??
+#                                       John W
